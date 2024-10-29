7a096568b12c93b020edfc6430d81596297a3683
Nethravathibs123 <nethranethra451@gmail.com>
2024-10-30 03:43:50 +0530

    first commit

---
+++ routes/password.js
@@ -0,0 +1,10 @@
+
+const express=require('express');
+const router=express.Router();
+const passwordController=require('../controller/passwordController');
+
+
+router.post('/forgotpassword',passwordController.forgotPassword);
+router.post('/resetpassword/:id',passwordController.resetPassword)
+
+module.exports=router;
